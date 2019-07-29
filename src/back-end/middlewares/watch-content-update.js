a7f99242813727e26b0808d303456957c12e0eae
Hamit Zor <thenrerise@gmail.com>
2019-07-29 17:11:08 +0300

    swap

--- src/back-end/middlewares/watch-content-update.js
+++ src/back-end/middlewares/watch-content-update.js
@@ -5,7 +5,7 @@ const SECRET = "3376DFA961EDFDF51E293370D298C0CDB8F06C4A15BAA336F2FFD9A10DFF5499
 const watchContentUpdate = () => async (req, res, next) => {
   const { secret } = req.params
   if (secret === SECRET) {
-    console.log(new Date(), 'Reloading content')
+    console.log(`[${new Date()}] Reloading content`)
     await loadContent()
     res.send("OK")
   }
